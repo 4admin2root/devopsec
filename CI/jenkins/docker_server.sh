b8cefe624e3cf8d5713c6f15c67e6e1d9c189873
jason <lvzhijun@synjones.com>
2017-09-02 09:12:43 +0800

    tested: jenkins_server

--- CI/jenkins/docker_server.sh
+++ CI/jenkins/docker_server.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 docker run -d -u root \
+--name jenkins
 -p 8080:8080 \
 -p 5000:5000 \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v /lib64:/lib64 \
 -v $(which docker):/bin/docker \
 -v /var/jenkins_home:/var/jenkins_home \
-jenkins
+4admin2root/jenkins:2.60.2
