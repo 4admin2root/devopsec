637c2b073fdcb32275334233f7655c7d0e957216
jason lui <piml.lui@gmail.com>
2017-09-07 14:08:46 +0800

    Create start_server.sh

---
+++ MONITOR/zabbix/start_server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+docker run --name zabbix-server --hostname zabbix-server \
+--link zabbix-mysql-server:mysql \
+-e DB_SERVER_HOST="mysql" \
+-e MYSQL_USER="zabbix" \
+-e MYSQL_PASSWORD="Passw0rd" \
+-v /etc/localtime:/etc/localtime:ro \
+-v /opt/docker/zabbix/alertscripts:/usr/lib/zabbix/alertscripts \
+-v /opt/docker/zabbix/externalscripts:/usr/lib/zabbix/externalscripts \
+-p 10051:10051 \
+-d \
+zabbix/zabbix-server-mysql
